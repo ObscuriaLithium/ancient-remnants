c5408bd4fdb0f4e2c6f65fff2890b619839efaf8
Obscuria <obscurialithium@gmail.com>
2026-09-06 15:54:06 -0300

    feat: add loot table extension for Pillager Outpost chests

--- fabric/src/main/java/dev/obscuria/remnants/fabric/FabricAncientRemnants.java
+++ fabric/src/main/java/dev/obscuria/remnants/fabric/FabricAncientRemnants.java
@@ -3,11 +3,21 @@ package dev.obscuria.remnants.fabric;
 import dev.obscuria.remnants.AncientRemnants;
 import dev.obscuria.remnants.AncientRemnantsHelper;
 import dev.obscuria.remnants.registry.AncientRemnantsItems;
+import dev.obscuria.remnants.registry.references.AncientRemnantsLootTables;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.NestedLootTable;
 
 public final class FabricAncientRemnants implements ModInitializer {
+
+    private static final ResourceKey<LootTable> PILLAGER_OUTPOST_CHEST;
 
     @Override
     public void onInitialize() {
@@ -18,5 +28,17 @@ public final class FabricAncientRemnants implements ModInitializer {
             output.accept(AncientRemnantsHelper.createCompleteCodex(output.getContext().holders()));
             AncientRemnantsHelper.createAllResearchScraps(output.getContext().holders()).forEach(output::accept);
         });
+
+        LootTableEvents.MODIFY.register((key, builder, _, _) -> {
+            if (!key.equals(PILLAGER_OUTPOST_CHEST)) return;
+            var reference = NestedLootTable.lootTableReference(AncientRemnantsLootTables.EXTENSION_PILLAGER_OUTPOST);
+            builder.withPool(LootPool.lootPool().add(reference));
+        });
+    }
+
+    static {
+        PILLAGER_OUTPOST_CHEST = ResourceKey.create(
+                Registries.LOOT_TABLE,
+                Identifier.withDefaultNamespace("chests/pillager_outpost"));
     }
 }
